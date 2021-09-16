ccaf594ba1c586a967dbbff478ac50d979856bc8
Lerppa20L04 <antila.leevi.04@gmail.com>
2021-09-16 19:30:58 +0300

    lobby kesken

---
+++ kodarit_monipeli_2021/Assets/Scripts/CreateJoinAndRoom.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+
+
+public class CreateJoinAndRoom : MonoBehaviourPunCallbacks
+{
+
+    public TMP_InputField createInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
