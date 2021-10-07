7bdf6a27988df557a22d4e8b886433a2212e1229
Lerppa20L04 <antila.leevi.04@gmail.com>
2021-10-07 19:27:55 +0300

    pelaaja liike aloitettu

--- kodarit_monipeli_2021/Assets/Scripts/PlayerController.cs
+++ kodarit_monipeli_2021/Assets/Scripts/PlayerController.cs
@@ -15,18 +15,41 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 3f;
     public float gravity = -9.81f;
 
+    private Vector3 velocity;
+
     [SerializeField] private bool isGround;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckIfGrouded();
+
+
+
     }
+
+    void CheckIfGrouded(){
+
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGround){
+
+            velocity.y = -2;
+
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
+
+    }
+
+
 }
