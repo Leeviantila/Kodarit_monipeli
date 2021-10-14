d82c907613873fba9078ea12908c704f9df93947
Lerppa20L04 <antila.leevi.04@gmail.com>
2021-10-14 19:28:59 +0300

    liikkuminen lisatty

---
+++ kodarit_monipeli_2021/Assets/Scripts/MouseLoop.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLoop : MonoBehaviour
+{
+
+    public float mouseSensitivity = 250f;
+    public float minxXAngle = -80f;
+    public float maxXAngle = 90f;
+    Transform playerBody;
+    private float mouseX;
+    private float mouse;
+    private float xRotation = 0f;
+    public CursorLockMode mode;
+
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- kodarit_monipeli_2021/Assets/Scripts/PlayerController.cs
+++ kodarit_monipeli_2021/Assets/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour
     public float gravity = -9.81f;
 
     private Vector3 velocity;
+    private Vector3 MoveDir;
 
     [SerializeField] private bool isGround;
 
@@ -30,13 +31,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckIfGrouded();
+        CheckIfGrounded();
 
-
+        Move();
 
     }
 
-    void CheckIfGrouded(){
+    void CheckIfGrounded(){
+        //Funktio tarkistaa onko pelaaja maassa
 
         isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -50,6 +52,29 @@ public class PlayerController : MonoBehaviour
         controller.Move(velocity*Time.deltaTime);
 
     }
+
+    void Move(){
+        //Funktio ohjaa pelaajan liikkumis ominaisuutta.
+        
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        // Toteuttaa if - else rakenteen (boolean muoto)
+        float targetSpeed = Input.GetButton("Fire1") ? runSpeed : moveSpeed;
+
+        MoveDir = transform.right * xAxis + transform.forward * zAxis;
+
+        if(MoveDir == Vector3.zero){
+            targetSpeed = 0;
+        }
+
+        controller.Move(MoveDir * targetSpeed * Time.deltaTime);
+
+
+
+
+    }
+
 
 
 }
